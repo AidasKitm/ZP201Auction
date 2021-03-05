4d1e542b4716379d646cc2db6db404002da8c40b
AidasKITM <aidas.puskunigis@kitm.lt>
2021-03-05 14:24:18 +0200

    Add fun explanation

---
+++ binarytest.py
@@ -0,0 +1,15 @@
+
+binary_code = 0o0111110
+#128 64 32 16 8 4 2 1
+# 0   0  0  1 0 0 1 0
+# 16+2 = 18
+# base2= 0,1. base10=0,1,2,3,4,5,6,7,8,9. base16=0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F
+number = chr(binary_code)
+print(number)
+
+
+# ASCII -> Abecele,skaicaii
+# UTF-8 -> naudoja 2 baitus-> daug simboliu, abeceliu -> 00101010 01011010
+# Zmoniu vardai -> store mysql -> Auto:UTF-8
+
+
