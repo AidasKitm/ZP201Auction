9def29233c407215d9d2fe8ef19cf92254448175
AidasKITM <aidas.puskunigis@kitm.lt>
2021-03-12 11:53:21 +0200

    Add socketIO functionality in auction_details

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from config import Config
 from models.user import User
 from models.auction import Auction
@@ -10,19 +10,50 @@ from views.login import Login
 from views.logout import LogOut
 from views.create_auction import CreateAuction
 from views.auctiondetail import AuctionDetails
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app=app)
+socketio = SocketIO(app=app)
 
-
-app.add_url_rule('/index', view_func=Index.as_view('index'))
+app.add_url_rule('/', view_func=Index.as_view('index'))
 app.add_url_rule('/register', view_func=Registration.as_view('register'))
 app.add_url_rule('/login', view_func=Login.as_view('login'))
 app.add_url_rule('/logout', view_func=LogOut.as_view('logout'))
 app.add_url_rule('/create_auction', view_func=CreateAuction.as_view('create_auction'))
 app.add_url_rule('/auction/<int:auction_id>', view_func=AuctionDetails.as_view('auction'))
 
+# SOCKET -> Tunelis -> komunikuoti tarp 2 prisijungimo tasku -> Server <-> Client
+
+@socketio.on('auction')
+def auction(response):
+    selected_auction = Auction.query.filter_by(id=response['auctionId']).first()
+    offers = Offer.query.filter_by(auction_id=selected_auction.id).order_by(Offer.price.desc()).limit(5).all()
+    biggest_offers = []
+    for offer in offers:
+        user = User.query.filter_by(id=offer.id).first()
+        offer_with_user = {'user': user, 'price': offer.price}
+        biggest_offers.append(offer_with_user)
+
+    auction_response = {'id':selected_auction.id, 'views': selected_auction.views, 'offers': biggest_offers}
+    emit('auctionResponse' + str(auction_response['id']), auction_response, broadcast=True)
+
+
 if __name__ == '__main__':
     db.create_all(app=app)
-    app.run()
+    socketio.run(app=app, host='0.0.0.0')
+
+# Python3 -> sintakse -> loginiu uzdaviniu pasprest
+
+# Vertinimo aplikacija:
+#
+# Vartotojas gali susikurti vartotojo sasaja.
+#
+# Įmonė gali susikurti sasaja
+#
+# Vartotojai gali įvertinti įmone ir palikti komentara prie įmonės įvertinimo.
+#
+# Vartotojas kuris paliko komentara gali ji pakoreguoti.
+#
+# Imone gali atsakyti i palikta vartotojo komentara.
--- forms/registration_form.py
+++ forms/registration_form.py
@@ -4,9 +4,10 @@ from wtforms.validators import DataRequired, Email
 from wtforms.fields.html5 import EmailField
 
 class RegistrationForm(FlaskForm):
-    first_name = StringField("Please input your first name", validators=[DataRequired("Don't leave empty field")])
-    last_name = StringField("Please input your last name", validators=[DataRequired("Don't leave empty field")])
-    email = EmailField("Please input your email", validators=[DataRequired(), Email()])
-    address = StringField("Please input your address", validators=[DataRequired("Don't leave empty field")])
-    username = StringField("Please input your username", validators=[DataRequired("Don't leave empty field")])
-    password = PasswordField("Input password", validators=[DataRequired("Bad password")])
+    first_name = StringField("First name", validators=[DataRequired("Don't leave empty field")])
+    last_name = StringField("Last name", validators=[DataRequired("Don't leave empty field")])
+    email = EmailField("Email", validators=[DataRequired(), Email()])
+    address = StringField("Address", validators=[DataRequired("Don't leave empty field")])
+    username = StringField("Username", validators=[DataRequired("Don't leave empty field")])
+    password = PasswordField("Password", validators=[DataRequired("Bad password")])
+
